35294181e12d17f3cb139012d9f03199a41b1c5c
dqhuy78 <dqhuy78@gmail.com>
2025-02-11 14:36:23 +0700

    Init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div
+    className="w-screen h-screen bg-neutral-950 flex items-center justify-center text-neutral-100"
+  >
+    <div className="w-96 bg-neutral-800 p-4 rounded-lg">
+      Your component go here
+    </div>
+  </div>
+</template>
